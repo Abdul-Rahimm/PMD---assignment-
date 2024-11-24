8f6af5347e182c338ee52cbf16134c7704969471
Abdul-Rahimm <k213951@nu.edu.pk>
2024-11-24 14:02:40 +0500

    cricket API working

--- src/app/(tabs)/cricket.tsx
+++ src/app/(tabs)/cricket.tsx
@@ -2,54 +2,51 @@ import { Button, StyleSheet } from "react-native";
 import React, { useState, useEffect } from "react";
 import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import { ScrollView } from "react-native";
 
 export default function TabOneScreen() {
   // State to store API data and loading state
   const [data, setData] = useState(null);
+  const [error, setError] = useState();
   const [loading, setLoading] = useState(true);
-  const [refresh, setRefresh] = useState(true);
-
-  const URL = "https://rapidapi.com/sportcontentapi/api/cricket-live-data/";
-  const headers = {
-    // "X-RapidAPI-Key": "your-rapidapi-key", // Replace with your actual key
-    "X-RapidAPI-Host": "sportcontentapi.p.rapidapi.com",
-  };
-
-  const fetchData = async () => {
-    try {
-      const response = await fetch(URL, {
-        method: "GET",
-        headers,
-      });
-
-      const textResponse = await response.text(); // Log the raw text response
-      console.log("Raw response:", textResponse);
-
-      const jsonResponse = JSON.parse(textResponse); // Parse to JSON
-      setData(jsonResponse);
-      console.log(data);
-      setLoading(false);
-    } catch (error) {
-      console.log("Error fetching cricket data : ", error);
-      setLoading(false);
-    }
-  };
 
   useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const url = "https://cricket-live-data.p.rapidapi.com/fixtures";
+        const options = {
+          method: "GET",
+          headers: {
+            "x-rapidapi-key":
+              "f89afba7b0mshedbc28a7540f903p113142jsn356174a6704f",
+            "x-rapidapi-host": "cricket-live-data.p.rapidapi.com",
+          },
+        };
+
+        const response = await fetch(url, options);
+        const result = await response.json();
+        setData(result.results);
+      } catch (err: any) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
     fetchData();
-  }, [refresh]);
+  }, []);
 
   return (
     <View style={styles.container}>
-      {loading ? (
-        <Text>Loading...</Text>
-      ) : data ? (
-        <Text>{JSON.stringify(data, null, 2)}</Text>
-      ) : (
-        <Text>No data available</Text>
-      )}
-
-      <Button title="Refresh" onPress={() => setRefresh(!refresh)} />
+      <ScrollView>
+        {loading ? (
+          <Text>Loading...</Text>
+        ) : data ? (
+          <Text>{JSON.stringify(data, null, 2)}</Text>
+        ) : (
+          <Text>No data available</Text>
+        )}
+      </ScrollView>
     </View>
   );
 }
